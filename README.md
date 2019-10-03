f0cd7e0974f5119eca2f18f1e47dadf0eda424e8
Apple <prophet_wang@foxitsoftware.cn>
2019-10-03 10:07:32 +0800

    add production config

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # flask
 flask
+
+# Docs
+    https://dormousehole.readthedocs.io/en/latest/installation.html#install-create-env
+# 开发环境
+    创建虚拟环境
+    python3 -m venv venv
+    激活虚拟环境
+    . venv/bin/activate
+    安装Flask
+    pip install Flask --default-timeout=1000
+
+# 开发服务器
+    https://dormousehole.readthedocs.io/en/latest/server.html#server
+    export FLASK_APP=my_application
+    export FLASK_ENV=development 开发环境
+    flask run
+# 部署服务器
+    https://dormousehole.readthedocs.io/en/latest/deploying/uwsgi.html#id1
+    wsgi uwsgi
---
+++ helloworld.py
@@ -0,0 +1,63 @@
+from flask import Flask, escape, url_for, request, render_template, make_response, abort
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    resp = make_response(render_template('hello.html'))
+    resp.set_cookie('username', 'the username')
+    return resp
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    username = request.cookies.get('username')
+    return render_template('hello.html', name=username)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'start login in'
+    else:
+        return 'show login form'
+
+@app.route('/send-email', methods=['POST'])
+def send_email():
+    if request.method == 'POST':
+        return 'sendEmail'
+
+@app.route('/projects/')
+def projects():
+    abort(401)
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+@app.route("/me")
+def me_api():
+
+    return {
+        "username": "321",
+        "theme": "darg"
+    }
+
+with app.test_request_context():
+    print(url_for('static', filename='style.css'))
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
